d53920f76b7f9adc625121f53fea11116cacd9da
Gerard Condon <git@gerardcondon.com>
2018-08-17 15:14:13 +0000

    Adding build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ # options explanation at http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
+git submodule init
+git submodule update
+
+cd website
+bundle
+middleman build
