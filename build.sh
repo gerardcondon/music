ddbf36f7416c68bbca6255095fa9964654d393d0
Gerard Condon <git@gerardcondon.com>
2018-08-17 15:32:03 +0000

    Adding git config to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
  # options explanation at http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
+
 git submodule init
 git submodule update
 
