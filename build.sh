7a0090d578a15d2e336a4a1d1e06fa7d276228fa
Gerard Condon <git@gerardcondon.com>
2018-08-17 15:43:00 +0000

    Adding -x to bash script

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
  # options explanation at http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
+set -x
 
-git config user.name "$USER_NAME"
-git config user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
 
 git submodule init
 git submodule update
